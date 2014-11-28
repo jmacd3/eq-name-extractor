c82d8ca5984eb7edb169dacf3b738e7622ca5d79
Justin Macdonald <jmacdonald@deseretdigital.com>
2014-11-28 12:28:41 -0700

    Fixes issues with qtip implementation

--- javascripts/extractor.js
+++ javascripts/extractor.js
@@ -1,7 +1,7 @@
-console.log('Hello world');
+
     function ShowToolTip(names) {
-        var groupEmailPopupTemplate = '<div id="ui-tooltip-7" class="ui-tooltip qtip ui-helper-reset ui-tooltip-medium ui-tooltip-shadow ui-tooltip-rounded ui-tooltip-pos-tr ui-widget ui-tooltip-focus" tracking="false" role="alert" aria-live="polite" aria-atomic="false" aria-describedby="ui-tooltip-7-content" aria-hidden="false" style="width: 280px; z-index: 15001; top: 191px; left: 372.087890625px; opacity: 1; display: block;"><div class="ui-tooltip-tip" style="border: 0px !important; height: 22px; width: 22px; top: 4px; right: -21px; background-color: transparent !important;"><canvas style="border: 0px !important; background-color: transparent !important;" height="22" width="22"></canvas></div><div class="ui-tooltip-content ui-widget-content" id="ui-tooltip-7-content" aria-atomic="true"><div class="emailGroupLinkPopup" style="display: block;">   <div class="emailGroupLinkPopupTitle">    <span>Email all Elders</span>   </div>   <div style="" class="emailGroupLinkPopupDescription">       </div>   <div class="emailGroupLinkPopupRadioBar">       </div>    <div class="emailGroupLinkPopupList" id="listItemELDER_EldersgroupNamelList"></div>   <div class="buttonBar" style="margin-right:auto; margin-left:auto; text-align:center; width:220px; padding:0px 20px 10px 20px">    <button id="emailGroupLink_Eldersselect_all">Select All</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    <button id="emailGroupLink_ElderscloseGroupEmailPopup" class="emailGroupLinkPopupButtonBar">Done</button>   </div>  </div></div></div>';
-        var control = $('#listItemELDER');
+        var groupEmailPopupTemplate = '<div id="ui-tooltip-7" class="ui-tooltip qtip ui-helper-reset ui-tooltip-medium ui-tooltip-shadow ui-tooltip-rounded ui-tooltip-pos-tr ui-widget ui-tooltip-focus" tracking="false" role="alert" aria-live="polite" aria-atomic="false" aria-describedby="ui-tooltip-7-content" aria-hidden="false" style="width: 280px; z-index: 15001; top: 191px; left: 372.087890625px; opacity: 1; display: block;"><div class="ui-tooltip-tip" style="border: 0px !important; height: 22px; width: 22px; top: 4px; right: -21px; background-color: transparent !important;"><canvas style="border: 0px !important; background-color: transparent !important;" height="22" width="22"></canvas></div><div class="ui-tooltip-content ui-widget-content" id="ui-tooltip-7-content" aria-atomic="true"><div class="emailGroupLinkPopup" style="display: block;">   <div class="emailGroupLinkPopupTitle">    <span>Copy all Elders</span>   </div>   <div style="" class="emailGroupLinkPopupDescription">       </div>   <div class="emailGroupLinkPopupRadioBar">       </div>    <div class="emailGroupLinkPopupList" id="EldersgroupNamelList"></div>   <div class="buttonBar" style="margin-right:auto; margin-left:auto; text-align:center; width:220px; padding:0px 20px 10px 20px">    <button id="eldersSelectAllGroupNamePopup">Select All</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    <button id="elderscloseGroupNamePopup" class="emailGroupLinkPopupButtonBar">Done</button>   </div>  </div></div></div>';
+        var control = $('#extractNamesGroupLink_Elders');
         control.qtip({
             content : {
                 text : groupEmailPopupTemplate
@@ -11,10 +11,10 @@ console.log('Hello world');
                 // Hide the tooltip when the close buttons in the dialogue is clicked
                 render : function (event, api) {
                     // Inject names
-                    $("#listItemELDER_EldersgroupNamelList").html(names.toString());
-                    $("#" + linkId + "closeGroupEmailPopup").click(api.destroy);
+                    $("#EldersgroupNamelList").html(names.toString());
+                    $("#elderscloseGroupNamePopup").click(api.destroy);
                     //listItemELDER_EldersgroupNamelList
-                    $("#" + linkId + "select_all").click(function() {
+                    $("#eldersSelectAllGroupNamePopup").click(function() {
                         var element = $("#listItemELDER_EldersgroupNamelList");
 
                         var doc = document
@@ -36,13 +36,17 @@ console.log('Hello world');
                 }
             },
             position : {
-                my : "top right",
-                at : "bottom left",
+                my : "top left",
+                at : "bottom right",
                 target : 'event',
                 adjust : {
                     screen : true,
                     scroll : true
                 }
+            },
+            show : {
+                event : "click",
+                solo : true
             },
             style : {
                 background : "#E8E5DE",
@@ -58,8 +62,11 @@ console.log('Hello world');
                 widget : true
             }
         });
-        control("show");
+
     }
+
+    // Inject new link
+    $("#emailGroupLink_Elders").after('<div id="emailGroupdiv">    <a id="extractNamesGroupLink_Elders" href="#" class="emailAddress" aria-describedby="ui-tooltip-4">Copy all Elders . . .</a>   </div>');
 
     // Extract names
     var namesRaw = $('.rosterName'),
@@ -67,6 +74,7 @@ console.log('Hello world');
     namesRaw.each(function( index ) {
         namesExtracted[index] = $( this ).text();
     });
+
 
     // Render tool tip
     ShowToolTip(namesExtracted);
